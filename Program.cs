f43df03a43564e97a3e91f7f0da7366a67a291ab
VictorGorban <VictorGorban2@ya.ru>
2018-10-06 18:53:59 +0300

    1

--- Program.cs
+++ Program.cs
@@ -6,14 +6,21 @@ namespace myApp
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 5; i++)
+            for (int j = 0; j < 5; j++)
             {
                 DoNothing();
                 System.Console.WriteLine(Hello.HelloWorld);
             }
+            int i = 0;
+
+            while (i < 4)
+            {
+                i++;
+                DoNothing();
+            }
         }
 
-        
+
         private static void DoNothing()
         {
 
